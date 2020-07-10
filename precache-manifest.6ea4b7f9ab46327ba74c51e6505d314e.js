e3b2b1a2af8fcd02bedb0a8abec0496e32e1a4d6
Vikash Kumar <vikashsmart1996@gmail.com>
2020-07-11 04:42:30 +0530

    Updates

---
+++ precache-manifest.6ea4b7f9ab46327ba74c51e6505d314e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457aff70c62ee895972a7f4b3489f001",
+    "url": "/react-spinners/index.html"
+  },
+  {
+    "revision": "6002d806cb1a2b92ea6e",
+    "url": "/react-spinners/static/css/main.71ca9613.chunk.css"
+  },
+  {
+    "revision": "af1b5b7e9f2bc0933560",
+    "url": "/react-spinners/static/js/2.ee76aa8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-spinners/static/js/2.ee76aa8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6002d806cb1a2b92ea6e",
+    "url": "/react-spinners/static/js/main.b4aeadd2.chunk.js"
+  },
+  {
+    "revision": "3b42cfdd8d179de4afa3",
+    "url": "/react-spinners/static/js/runtime-main.b2cfe56b.js"
+  }
+]);
